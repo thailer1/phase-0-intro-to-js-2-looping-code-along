441aace7de30e3abe6b8fd32903e8c04f98e1181
Thailer <thailer.palmer@gmail.com>
2023-02-08 15:59:18 -0500

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const people = ["Guadalupe", "Ollie", "Aki"];
+const message = []
+
+function writeCards(people) {
+  for (let i = 0; i < people.length; i++) {
+    message.push(`Thank you, ${people[i]}, for the wonderful surprise gift!`);
+    debugger;
+  }
+
+  return message;
+}
+
+// writeCards(people);
+
+
+let number = 11;
+function countDown(number){
+while (number >= 0) {
+  console.log(number--);
+}
+  return number
+  debugger;
+}
